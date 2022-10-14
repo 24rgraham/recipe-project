44b3fd75715fe211cab6f58016a2a920773c7574
Pooja Agarwal <pooja@Poojas-MacBook-Air.local>
2022-10-14 07:50:58 -0700

    adding seperate functions for each section

--- assets/script.js
+++ assets/script.js
@@ -6,69 +6,183 @@ const options = {
     }
 };
 
-var description;
+var globalRecipe;
+var globalDescription;
+var globalTopDescription;
+var globalInstructions = [];
+var globalIngredients = [];
 
-
-function fetchRandomRecipe() {
+function getRandomRecipe() {
     var recipeNumber = Math.floor(Math.random() * 100)
-    console.log(recipeNumber);
     var url = "https://tasty.p.rapidapi.com/recipes/list?from=" + recipeNumber + "&size=1";
 
-    fetch(url, options)
-	.then(response => response.json())
-	.then(function (data) {
-        console.log(data);
-        console.log(data.results[0].name + " " + data.results[0].thumbnail_url+ " " + data.results[0].description);
-
-        recipeNameEl.textContent = "Today's recipe: " + data.results[0].name;
-        let recipeImageEl = document.createElement("img");
-        recipeImageEl.src = data.results[0].thumbnail_url;
-        recipeImageEl.className = "recipeImage";
-        recipeImageDivEl.appendChild(recipeImageEl);
-
-        description = data.results[0].description;
-        recipedetailEl.textContent = "description:"  + description;
-        var instructionArray = [];
-        if (data.results[0].instructions) {
-            console.log("instruction in result");
-            instructionArray = data.results[0].instructions;
-        } else if (data.results[0].recipes[0].instructions) {
-            console.log("instruction in recipes");
-            instructionArray = data.results[0].recipes[0].instructions;
-        }
-        for(var i=0 ; i < instructionArray.length ; i++) {
-            var instruction = instructionArray[i].display_text;
-            console.log(instruction);
-            //instructiondetailEl.textContent = "preparation steps:" + instruction;
-          let intstep = document.createElement("p");
-          intstep.textContent=i + ": " + instruction;
-          instructiondetailEl.appendChild(intstep);
-        }
-        var ingarr =[];
-         ingarr = data.results[0].sections[0].components;
-        for(var i=0 ; i<ingarr.length ;i++);
-        var inglist=ingarr[i].raw_text;
-        console.log(inglist);
-    })
-	.catch(err => console.error(err));
+    return fetch(url, options)
+    .then(response => response.json())
+    .then(function (data) {
+        console.log(data.results[0]);
+        return data.results[0];
+    });
 }
 
-function handleButtonEvent(event) {
-    event.preventDefault();
-    console.log("des" + description);
-    //console.log("ins" + instruction);
-    for (var i=0; i<instructionArr.length;i++) {
-        console.log(instructionArr[i]);
+function getRecipeName() {
+    return globalRecipe.name;
+}
+
+function printRecipeName(name) {
+    console.log("Name: " + name);
+    recipeNameEl.textContent = name;
+}
+
+function getRecipeImage() {
+    return globalRecipe.thumbnail_url;
+}
+
+function printRecipeImage(image_url) {
+    console.log("Image: " + image_url);
+    let recipeImageEl = document.createElement("img");
+    recipeImageEl.src = image_url;
+    recipeImageEl.className = "recipeImage";
+    recipeImageDivEl.appendChild(recipeImageEl);
+}
+
+function getRecipeDescription() {
+    return globalRecipe.description;
+}
+
+function printRecipeDescription() {
+    console.log("Description: " + globalTopDescription + " " + globalDescription);
+    if (globalDescription || globalTopDescription) {
+        //recipeDescriptionEl.innerHTML = "<p>Description</p>" + "<p>" + globalDescription + "</p>";
+        let descriptionHeading = document.createElement("h2");
+        descriptionHeading.textContent = "Description:";
+        recipeDescriptionEl.appendChild(descriptionHeading);
+
+        let description = document.createElement("p");
+        if (globalDescription) {
+            description.innerHTML = globalDescription;
+        } else {
+            description.innerHTML = globalTopDescription;
+        }
+        
+        recipeDescriptionEl.appendChild(description);
     }
-    recipedetailEl.textContent = "description:"  + description;
-    instructiondetailEl.textContent = "preparation steps:" + instructionArr[i];
 }
 
-var recipeNameEl = document.querySelector(".todayrecipe");
-var recipeImageDivEl = document.querySelector(".recipeDiv");
-var buttonEl = document.getElementById("button");
-buttonEl.addEventListener('click', handleButtonEvent);
-var recipedetailEl = document.querySelector(".recipedetail");
-var instructiondetailEl = document.querySelector(".instructiondetail");
+function getRecipeInstructions() {
+    var instructions = new Array();
+    if (globalRecipe.instructions && globalRecipe.instructions.length > 0) {
+        for (i=0; i<globalRecipe.instructions.length; i++) {
+            instructions.push(globalRecipe.instructions[i].display_text);
+        }
+    }
+    return instructions;
+}
 
-fetchRandomRecipe();
+function printRecipeInstructions() {
+    console.log("Instructions");
+    let instructionsHeading = document.createElement("h2");
+    instructionsHeading.textContent = "Instructions:";
+    recipeInstructionsEl.appendChild(instructionsHeading);
+
+    for (i=0; i<globalInstructions.length; i++) {
+        if (globalInstructions[i]) {
+            console.log(globalInstructions[i]);
+            let instruction = document.createElement("p");
+            instruction.textContent = globalInstructions[i];
+            recipeInstructionsEl.appendChild(instruction);
+        }
+    }
+}
+
+function getRecipeIngredients() {
+    var ingredients = new Array();
+    if (globalRecipe.sections && globalRecipe.sections.length > 0) {
+        if (globalRecipe.sections[0].components && globalRecipe.sections[0].components.length > 0) {
+            for (i=0; i<globalRecipe.sections[0].components.length; i++) {
+                ingredients.push(globalRecipe.sections[0].components[i].raw_text);
+            }
+        }
+    }
+    return ingredients;
+}
+
+function printRecipeIngredients(ingredients) {
+    console.log("Ingredients");
+    let ingredientHeading = document.createElement("h2");
+    ingredientHeading.textContent = "Ingredients:";
+    recipeIngredientsEl.appendChild(ingredientHeading);
+
+    for (i=0; i<globalIngredients.length; i++) {
+        if (globalIngredients[i]) {
+            console.log(globalIngredients[i]);
+            let ingredient = document.createElement("p");
+            ingredient.textContent = globalIngredients[i];
+            recipeIngredientsEl.appendChild(ingredient);
+        }
+    }
+}
+
+function loadRecipe() {
+    this.getRandomRecipe()
+    .then((recipe) => {
+        globalTopDescription = recipe.description;
+        if (recipe.recipes && recipe.recipes.length > 0) {
+            return recipe.recipes[0];
+        } else {
+            return recipe;
+        }
+    })
+    .then((recipe) => {
+        globalRecipe = recipe;
+        var name = getRecipeName();
+        printRecipeName(name);
+
+        var image_url = getRecipeImage();
+        printRecipeImage(image_url);
+
+        globalDescription = getRecipeDescription();
+        globalIngredients = getRecipeIngredients();
+        globalInstructions = getRecipeInstructions();
+    });
+}
+
+function handleMoreInfoButtonEvent() {
+    console.log("In handleMoreInfoButtonEvent");
+    printRecipeDescription();
+
+    printRecipeInstructions();
+
+    printRecipeIngredients();
+}
+
+function handleNextRecipeButtonEvent() {
+    console.log("In handleNextRecipeButtonEvent");
+    clearElements();
+    loadRecipe();
+}
+
+function clearElements() {
+    recipeImageDivEl.innerHTML = "";
+    recipeDescriptionEl.innerHTML = "";
+    recipeInstructionsEl.innerHTML = "";
+    recipeIngredientsEl.innerHTML = "";
+}
+
+var recipeNameEl = document.querySelector(".recipeName");
+
+var recipeImageDivEl = document.querySelector(".recipeImageDiv");
+
+var moreInfoButtonEl = document.getElementById("moreInfoButton");
+
+var nextRecipeButtonEl = document.getElementById("nextRecipeButton");
+
+var recipeDescriptionEl = document.querySelector(".recipeDescription");
+
+var recipeInstructionsEl = document.querySelector(".recipeInstructionsDiv");
+
+var recipeIngredientsEl = document.querySelector(".recipeIngredientsDiv");
+
+moreInfoButtonEl.addEventListener('click', handleMoreInfoButtonEvent);
+nextRecipeButtonEl.addEventListener('click', handleNextRecipeButtonEvent);
+
+loadRecipe();
